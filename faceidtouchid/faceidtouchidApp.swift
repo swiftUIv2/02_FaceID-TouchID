3fcd8ceff0917f5d7a5ccdafae30ffe9c5663af5
emm <723125@gmail.com>
2020-12-20 14:13:18 +0000

    Initial Commit

---
+++ faceidtouchid/faceidtouchidApp.swift
@@ -0,0 +1,17 @@
+//
+//  faceidtouchidApp.swift
+//  faceidtouchid
+//
+//  Created by emm on 20/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct faceidtouchidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
